ffb6e456b77384f6623a10f000845ecc60ca2035
JYCHO <cjy0029@naver.com>
2022-03-04 21:35:47 +0900

    config : storybook setting 완료

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,20 @@
 export default {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    quotes: ['error', 'double'],
-  },
+    quotes: ['error', 'double']
+  }
 };
+export const extends = ["plugin:storybook/recommended"];
