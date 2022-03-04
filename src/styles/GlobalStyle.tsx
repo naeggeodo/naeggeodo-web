91b33c61f94f6ef868e4f64c504da3fe5b10f93b
JYCHO <cjy0029@naver.com>
2022-03-04 21:21:29 +0900

    config : styled-components 추가

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle, css } from 'styled-components';
+import reset from 'styled-reset';
+
+const globalStyle = css`
+  ${reset}
+  html {
+    font-size: 10px;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+  ${globalStyle};
+`;
+
+export default GlobalStyle;
