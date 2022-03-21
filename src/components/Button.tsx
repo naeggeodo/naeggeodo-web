2bf8f71fe2d945f74589fa6136a909c2ea55d3f1
JYCHO <cjy0029@naver.com>
2022-03-21 21:31:09 +0900

    chore : testing storybook deploy

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Button = () => {
-  return <button>Button</button>;
+  return <StyledButton>Button</StyledButton>;
 };
+
+const StyledButton = styled.button`
+  background-color: pink;
+  border: none;
+  border-radius: 5px;
+  width: 300px;
+  height: 40px;
+  color: white;
+`;
 
 export default Button;
