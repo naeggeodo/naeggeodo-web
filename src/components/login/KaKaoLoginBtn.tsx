ddc2a10e2eb4018caf349521b18a0f81111517f3
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-03-24 12:14:55 +0900

    config : login logo svg file

--- src/components/login/KaKaoLoginBtn.tsx
+++ src/components/login/KaKaoLoginBtn.tsx
@@ -1,15 +1,13 @@
 import styled from 'styled-components';
+import Image from 'next/image';
 import palette from '../../styles/palette';
+import KaKaologo from '../../assets/icons/kakaologo.svg';
+
 const KaKaoLoginBtn = () => {
   return (
     <Wrap>
-      <Icon viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
-        <path
-          d='M8.99671 0.875C4.16663 0.875 0.25 3.98236 0.25 7.81697C0.25 10.2962 1.88631 12.4681 4.35064 13.7011L3.46348 16.8911C3.46348 16.8911 3.44705 17.0399 3.54234 17.0961C3.63763 17.1523 3.75263 17.1093 3.75263 17.1093C4.02863 17.0696 6.93982 15.0102 7.44912 14.6532C7.95184 14.7259 8.47428 14.7622 9.00329 14.7622C13.8334 14.7622 17.75 11.6549 17.75 7.82027C17.75 3.98566 13.8268 0.875 8.99671 0.875Z'
-          fill='black'
-        />
-      </Icon>
-      카카오 계정으로 로그인
+      <Image src={KaKaologo} alt='kakao logo' />
+      <Span>카카오 계정으로 로그인</Span>
     </Wrap>
   );
 };
@@ -28,10 +26,8 @@ const Wrap = styled.div`
   font-size: 17px;
   margin-bottom: 6px;
   cursor: pointer;
-  margin-bottom: 3%;
+  margin-bottom: 10px;
 `;
-const Icon = styled.svg`
-  width: 18px;
-  height: 18px;
-  margin-right: 6px;
+const Span = styled.span`
+  margin-left: 6px;
 `;
