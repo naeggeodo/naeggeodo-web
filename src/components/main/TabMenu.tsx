1b348dfc32abee9e65b3fb0cf61b2cf61a0d2e31
JYCHO <cjy0029@naver.com>
2022-03-27 17:41:15 +0900

    feat : 메인페이지 모바일 탭 ui 구현

--- src/components/main/TabMenu.tsx
+++ src/components/main/TabMenu.tsx
@@ -10,24 +10,34 @@ const TabMenu = () => {
   return (
     <Container>
       <TabButton>
-        <img src={homeblack} alt='홈버튼' />
-        <p>홈</p>
+        <div>
+          <img src={homeblack} alt='홈버튼' />
+          <p>홈</p>
+        </div>
       </TabButton>
       <TabButton>
-        <img src={searchgray} alt='검색' />
-        <p>검색</p>
+        <div>
+          <img src={searchgray} alt='검색' />
+          <p>검색</p>
+        </div>
       </TabButton>
       <TabButton>
-        <img src={plusgray} alt='추가하기' />
-        <p>내꺼도 생성</p>
+        <div>
+          <img src={plusgray} alt='추가하기' />
+          <p>내꺼도 생성</p>
+        </div>
       </TabButton>
       <TabButton>
-        <img src={chatgray} alt='채팅방' />
-        <p>내꺼도</p>
+        <div>
+          <img src={chatgray} alt='채팅방' />
+          <p>내꺼도</p>
+        </div>
       </TabButton>
       <TabButton>
-        <img src={moregray} alt='더보기' />
-        <p>전체</p>
+        <div>
+          <img src={moregray} alt='더보기' />
+          <p>전체</p>
+        </div>
       </TabButton>
     </Container>
   );
@@ -35,8 +45,7 @@ const TabMenu = () => {
 
 const Container = styled.div`
   display: flex;
-  justify-content: space-between;
-  padding: 0px 20px;
+  justify-content: space-evenly;
   height: 83px;
   background: #ffffff;
   box-shadow: 0px -0.5px 0px #e0e0e0;
@@ -48,7 +57,27 @@ const TabButton = styled.button`
   border: none;
   background-color: transparent;
   cursor: pointer;
-  & > p {
+
+  &:nth-of-type(-n + 4) > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 50px;
+
+    & > img {
+      height: 24px;
+    }
+  }
+
+  &:nth-of-type(5) > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 50px;
+    & > img {
+      height: 6px;
+      transform: translateY(160%);
+    }
   }
 `;
 
