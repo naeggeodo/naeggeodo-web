fccc7fc885e6fe0721008116f4c669f80f340158
JYCHO <cjy0029@naver.com>
2022-04-04 18:35:22 +0900

    refactor : next-plugin-compose 라이브러리로 next.config.js 파일 리팩토링

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,20 +2,26 @@ import { createGlobalStyle, css } from 'styled-components';
 import reset from 'styled-reset';
 import SpoqaHanSansNeoBold from '../assets/fonts/SpoqaHanSansNeo-Bold.woff';
 import SpoqaHanSansNeoRegular from '../assets/fonts/SpoqaHanSansNeo-Regular.woff';
+import SpoqaHanSansNeoBold2 from '../assets/fonts/SpoqaHanSansNeo-Bold.woff2';
+import SpoqaHanSansNeoRegular2 from '../assets/fonts/SpoqaHanSansNeo-Regular.woff2';
 
 const globalStyle = css`
   ${reset}
 
   @font-face {
     font-family: 'SpoqaBold';
-    src: url(${SpoqaHanSansNeoBold}) format('woff');
+    src: local('SpoqaHanSansNeo-Bold'),
+      url(${SpoqaHanSansNeoBold2}) format('woff2'),
+      url(${SpoqaHanSansNeoBold}) format('woff');
     font-display: fallback;
   }
 
   @font-face {
     font-family: 'Spoqa';
+    src: local('SpoqaHanSansNeo-Regular'),
+      url(${SpoqaHanSansNeoRegular2}) format('woff2'),
+      url(${SpoqaHanSansNeoRegular}) format('woff');
     font-display: fallback;
-    src: url(${SpoqaHanSansNeoRegular}) format('woff');
   }
 
   html {
