9f4c63df715f4164f7cbf367044872eeaf452ee6
JYCHO <cjy0029@naver.com>
2022-04-04 16:20:38 +0900

    config : 스토리북 뷰포트 수정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -18,6 +18,44 @@ export const decorators = [
   ),
 ];
 
+const customViewports = {
+  iPhone5: {
+    name: 'IPhone5',
+    styles: {
+      width: '320px',
+      height: '640px',
+    },
+  },
+  iPhone12Pro: {
+    name: 'IPhone12 Pro',
+    styles: {
+      width: '390px',
+      height: '844px',
+    },
+  },
+  iPhoneSE: {
+    name: 'IPhone SE',
+    styles: {
+      width: '375px',
+      height: '667px',
+    },
+  },
+  iPadAir: {
+    name: 'IPad Air',
+    styles: {
+      width: '820px',
+      height: '1180px',
+    },
+  },
+  iPadMini: {
+    name: 'IPad Mini',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -25,5 +63,13 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  options: {
+    storySort: {
+      method: 'alphabetical',
+    },
+  },
+  viewport: {
+    viewports: customViewports,
   },
 };
