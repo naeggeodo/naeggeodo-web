01d668cd798d8f70f31bacda08ca33a7fd6ae8fe
JYCHO <cjy0029@naver.com>
2022-04-07 16:16:13 +0900

    config : next, redux, saga 세팅 완료

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,43 @@
-import { combineReducers } from 'redux';
-import counter from './counter';
-const rootReducer = combineReducers({ counter });
-export default rootReducer;
+import createSagaMiddleware from '@redux-saga/core';
+import { createWrapper, HYDRATE } from 'next-redux-wrapper';
+import { combineReducers, applyMiddleware, createStore } from 'redux';
+import { all, fork } from 'redux-saga/effects';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+import counter from './counter';
+
+//** RootReducer */
+export const rootReducer = combineReducers({ counter });
+
+//** RootSaga */
+export function* rootSaga() {
+  yield all([]);
+}
+
+//** RootReducer Types */
 export type RootState = ReturnType<typeof rootReducer>;
+
+// Hydrate
+const reducer = (state, action) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state,
+      ...action.payload,
+    };
+    return nextState;
+  }
+  return rootReducer(state, action);
+};
+
+// 스토어 타입
+export type RootState = ReturnType<typeof rootReducer>;
+
+export const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+  return store;
+};
+export const wrapper = createWrapper(configureStore, { debug: true });
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,14 @@
 import { AppProps } from 'next/app';
 import GlobalStyle from '../styles/GlobalStyle';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from '../modules';
-import { composeWithDevTools } from 'redux-devtools-extension';
-const store = createStore(rootReducer, composeWithDevTools());
+import { wrapper } from '../modules';
+
 const app = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Provider store={store}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </Provider>
+      <GlobalStyle />
+      <Component {...pageProps} />
     </>
   );
 };
 
-export default app;
+export default wrapper.withRedux(app);
