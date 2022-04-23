70da9990a6301f9423f9ccfb0a4de2283ae4a000
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-04-23 13:47:19 +0900

    config : chatting send error

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -1,5 +1,8 @@
 import { CompatClient } from '@stomp/stompjs';
 import { ChatItemType } from '../../types/dataTypes';
+import { Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
 export function useChat() {
   const connect = (
     stompClient: CompatClient,
@@ -7,7 +10,9 @@ export function useChat() {
     setMessageList: React.Dispatch<React.SetStateAction<ChatItemType[]>>,
   ) => {
     stompClient.connect({ chatMain_id: '1', sender: '1' }, () => {
+      console.log('---------연결------------');
       stompClient.subscribe(`/topic/${roomId}`, (data) => {
+        console.log('---------구독------------');
         const newMessage = JSON.parse(data.body);
         setMessageList((prev) => prev.concat(newMessage));
       });
@@ -22,10 +27,11 @@ export function useChat() {
     //   stompClient.connect({ chatMain_id: '1', sender: '1' }, () => {
     //     stompClient.send('/app/chat/send', {}, JSON.stringify(data));
     //   });
-    // } else {
-    stompClient.send('/app/chat/send', {}, JSON.stringify(data));
-    // 두번째 전송시 연결이 끊기는 문제
     // }
+    console.log(stompClient.connected);
+    if (stompClient && data) {
+      stompClient.send('/app/chat/send', {}, JSON.stringify(data));
+    } // 두번째 전송시 연결이 끊기는 문제
     setMessage('');
   };
 
