217f9d0c588cc4aaac16e63ce460df2ccceb209d
JYCHO <cjy0029@naver.com>
2022-04-25 00:51:12 +0900

    refactor : axios instance 생성및 모듈화

--- src/service/api/chatting/ChattingService.ts
+++ src/service/api/chatting/ChattingService.ts
@@ -1,11 +1,9 @@
-import axios from 'axios';
+import { ApiService } from '..';
 
 export default class ChattingService {
   static async asyncGetChattingRoomInfo(chattingRoomId: any) {
     try {
-      const res = await axios.get(
-        `http://15.165.248.39:9090/chat/rooms/${chattingRoomId}`,
-      );
+      const res = await ApiService.getApi(`/chat/rooms/${chattingRoomId}`);
 
       return res;
     } catch (err) {
