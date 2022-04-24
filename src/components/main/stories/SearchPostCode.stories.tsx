a646b2012595f74bddcb54400589a56ded52090f
JYCHO <cjy0029@naver.com>
2022-04-24 15:18:21 +0900

    feat : 우편 번호 검색 파트 스토리 추가

---
+++ src/components/main/stories/SearchPostCode.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta } from '@storybook/react';
+import SearchPostCode from '../SearchPostCode';
+
+export default {
+  title: 'main/컴포넌트',
+  component: SearchPostCode,
+  decorators: [
+    (Story) => (
+      <div>
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof SearchPostCode>;
+
+export const SearchPostCodeStory = () => <SearchPostCode />;
+
+SearchPostCodeStory.storyName = '우편 번호 검색';
