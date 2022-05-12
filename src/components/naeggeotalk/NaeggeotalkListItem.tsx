cdabde4c3119499e2aa547ee67768ad9dde91d17
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-05-13 00:47:19 +0900

    feat : 채팅방 햄버거 서랍 ui

--- src/components/naeggeotalk/NaeggeotalkListItem.tsx
+++ src/components/naeggeotalk/NaeggeotalkListItem.tsx
@@ -1,28 +1,41 @@
 import Image from 'next/image';
 import styled from 'styled-components';
 
+type StyledType = {
+  active?: boolean;
+};
+
 const NaeggeotalkListItem = ({ data }: any) => {
   return (
-    <Wrap>
-      <Image src='/assets/images/hamburger.svg' width={44} height={44} />
-      <div>
-        <Title>{data.title}</Title>
-        <Date>{data.date}</Date>
-        <Populate>{data.populate}</Populate>
-      </div>
-    </Wrap>
+    <Container active={true}>
+      <Content>
+        <div>
+          <Image src='/assets/images/hamburger.svg' width={44} height={44} />
+          <div>
+            <Title>{data.title}</Title>
+            <Date>{data.date}</Date>
+            <Populate>{data.populate}</Populate>
+          </div>
+        </div>
+        {/* <Image src='/assets/images/yellowstar.svg' width={18} height={24} /> */}
+        <Image src='/assets/images/graystar.svg' width={18} height={24} />
+      </Content>
+    </Container>
   );
 };
 
-const Wrap = styled.div`
-  width: 100%;
+const Container = styled.div<StyledType>`
+  /* width: 100vw; */
+  /* border: 1px solid blue; */
+  border-bottom: 1px solid #f2f2f8;
+  background: ${(props) => (props.active ? ' #f2f2f8;' : '#fff')};
+`;
 
+const Content = styled.div`
   display: flex;
+  justify-content: space-between;
 
   padding: 20px 0;
-  & > div {
-    margin-left: 10px;
-  }
 `;
 
 const Title = styled.h3`
