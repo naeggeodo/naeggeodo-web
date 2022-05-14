5ad9d420b3de3707e5aeccc5d9402169ccac22e1
JYCHO <cjy0029@naver.com>
2022-05-14 23:53:52 +0900

    fix : handlers msw api 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,25 +3,64 @@ import { rest } from 'msw';
 // ** npx msw init ./public --save
 
 export const handlers = [
-  rest.get('https://my.backend/book', (req, res, ctx) => {
+  rest.get(`${process.env.NEXT_PUBLIC_API_URL}/categories`, (req, res, ctx) => {
     return res(
       ctx.json({
-        title: 'Lord of the Rings',
-        imageUrl: '/book-cover.jpg',
-        description:
-          'The Lord of the Rings is an epic high-fantasy novel written by English author and scholar J. R. R. Tolkien.',
+        categories: [
+          {
+            idx: 0,
+            category: 'ALL',
+          },
+          {
+            idx: 1,
+            category: 'CHICKEN',
+          },
+          {
+            idx: 2,
+            category: 'JAPANESE',
+          },
+          {
+            idx: 3,
+            category: 'CHINESE',
+          },
+          {
+            idx: 4,
+            category: 'KOREAN',
+          },
+          {
+            idx: 5,
+            category: 'SNACKS',
+          },
+          {
+            idx: 6,
+            category: 'STEW',
+          },
+          {
+            idx: 7,
+            category: 'PIZZA',
+          },
+          {
+            idx: 8,
+            category: 'WESTERN',
+          },
+          {
+            idx: 9,
+            category: 'GRILLED_MEAT',
+          },
+          {
+            idx: 10,
+            category: 'PORK_FEET',
+          },
+          {
+            idx: 11,
+            category: 'DESSERT',
+          },
+          {
+            idx: 12,
+            category: 'FASTFOOD',
+          },
+        ],
       }),
-    );
-  }),
-  rest.get('/reviews', (req, res, ctx) => {
-    return res(
-      ctx.json([
-        {
-          id: '60333292-7ca1-4361-bf38-b6b43b90cb16',
-          author: 'John Maverick',
-          text: 'Lord of The Rings, is with no absolute hesitation, my most favored and adored book by‑far. The triology is wonderful‑ and I really consider this a legendary fantasy series. It will always keep you at the edge of your seat‑ and the characters you will grow and fall in love with!',
-        },
-      ]),
     );
   }),
 ];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { END } from 'redux-saga';
 import MainTemplate from '../components/main/MainTemplate';
 import { wrapper } from '../modules';
