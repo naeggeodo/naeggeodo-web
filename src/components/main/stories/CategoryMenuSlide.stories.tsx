52614299d083ac8439b4f27a39b18b3ad4af43d0
JYCHO <cjy0029@naver.com>
2022-05-15 00:55:48 +0900

    fix : msw 설정 수정

--- src/components/main/stories/CategoryMenuSlide.stories.tsx
+++ src/components/main/stories/CategoryMenuSlide.stories.tsx
@@ -1,27 +1,22 @@
 import CategoryMenuSlide from '../CategoryMenuSlide';
-import { ComponentMeta } from '@storybook/react';
-import { categoryMockData } from '../data';
+import { ComponentMeta, Story } from '@storybook/react';
+import { rest } from 'msw';
+import { configureStore } from '../../../modules';
+import { handlers } from '../../../mocks/handlers';
 
 export default {
   title: 'main/컴포넌트',
   component: CategoryMenuSlide,
-  decorators: [
-    (Story) => (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        }}>
-        <Story />
-      </div>
-    ),
-  ],
+  decorators: [(Story) => <Story />],
 } as ComponentMeta<typeof CategoryMenuSlide>;
 
-export const CategoryMenu = () => (
-  <CategoryMenuSlide categories={categoryMockData} />
-);
+const Template = (args) => <CategoryMenuSlide {...args} />;
 
-CategoryMenu.storyName = '카테고리 메뉴 슬라이드';
+export const CategoryMenuStory = Template.bind({});
+CategoryMenuStory.args = {};
+
+CategoryMenuStory.parameters = {
+  msw: handlers,
+};
+
+CategoryMenuStory.storyName = '카테고리 메뉴 슬라이드';
