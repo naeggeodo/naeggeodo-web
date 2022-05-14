0d019955abe1407de3bb26d2a409ded3f9e6c202
JYCHO <cjy0029@naver.com>
2022-05-15 01:24:55 +0900

    fix : 카테고리 args 추가

--- src/components/main/stories/CategoryMenuSlide.stories.tsx
+++ src/components/main/stories/CategoryMenuSlide.stories.tsx
@@ -1,7 +1,5 @@
 import CategoryMenuSlide from '../CategoryMenuSlide';
-import { ComponentMeta, Story } from '@storybook/react';
-import { rest } from 'msw';
-import { configureStore } from '../../../modules';
+import { ComponentMeta } from '@storybook/react';
 import { handlers } from '../../../mocks/handlers';
 
 export default {
@@ -13,7 +11,63 @@ export default {
 const Template = (args) => <CategoryMenuSlide {...args} />;
 
 export const CategoryMenuStory = Template.bind({});
-CategoryMenuStory.args = {};
+
+CategoryMenuStory.args = {
+  foodCategories: [
+    {
+      idx: 0,
+      category: 'ALL',
+    },
+    {
+      idx: 1,
+      category: 'CHICKEN',
+    },
+    {
+      idx: 2,
+      category: 'JAPANESE',
+    },
+    {
+      idx: 3,
+      category: 'CHINESE',
+    },
+    {
+      idx: 4,
+      category: 'KOREAN',
+    },
+    {
+      idx: 5,
+      category: 'SNACKS',
+    },
+    {
+      idx: 6,
+      category: 'STEW',
+    },
+    {
+      idx: 7,
+      category: 'PIZZA',
+    },
+    {
+      idx: 8,
+      category: 'WESTERN',
+    },
+    {
+      idx: 9,
+      category: 'GRILLED_MEAT',
+    },
+    {
+      idx: 10,
+      category: 'PORK_FEET',
+    },
+    {
+      idx: 11,
+      category: 'DESSERT',
+    },
+    {
+      idx: 12,
+      category: 'FASTFOOD',
+    },
+  ],
+};
 
 CategoryMenuStory.parameters = {
   msw: handlers,
