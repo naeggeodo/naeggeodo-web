9f369af39c6ceb9f1c3fcde29cfd6e58d53b347b
JYCHO <cjy0029@naver.com>
2022-05-17 03:33:25 +0900

    fix : apple login button 제거

--- src/components/login/LoginTemplate.tsx
+++ src/components/login/LoginTemplate.tsx
@@ -1,6 +1,5 @@
 import styled, { css } from 'styled-components';
 import Image from 'next/image';
-import AppleLoginBtn from '../login/AppleLoginBtn';
 import KaKaoLoginBtn from '../login/KaKaoLoginBtn';
 import NaverLoginBtn from '../login/NaverLoginBtn';
 import palette from '../../styles/palette';
@@ -13,25 +12,27 @@ const LoginTemplate = () => {
   return (
     <Container>
       <ContentWrap>
-        <StyledWrapper name='header'>
-          <Image
-            src='/assets/images/prevbtn.svg'
-            alt='prev button'
-            width={14}
-            height={24}
-          />
-        </StyledWrapper>
-        <StyledWrapper name='content'>
-          <Title>로그인하여</Title>
-          <Title>내꺼도의 모든 서비스를</Title>
-          <Title>이용하세요.</Title>
-          <P>우리동네 배달비 반값 플랫폼</P>
-        </StyledWrapper>
+        <TextWrapper>
+          <StyledWrapper name='header'>
+            <Image
+              src='/assets/images/prevbtn.svg'
+              alt='prev button'
+              width={14}
+              height={24}
+            />
+          </StyledWrapper>
+
+          <StyledWrapper name='content'>
+            <Title>로그인하여</Title>
+            <Title>내꺼도의 모든 서비스를</Title>
+            <Title>이용하세요.</Title>
+            <P>우리동네 배달비 반값 플랫폼</P>
+          </StyledWrapper>
+        </TextWrapper>
 
         <StyledWrapper name='buttons'>
           <KaKaoLoginBtn />
           <NaverLoginBtn />
-          <AppleLoginBtn />
         </StyledWrapper>
       </ContentWrap>
     </Container>
@@ -41,15 +42,25 @@ const LoginTemplate = () => {
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: #fff;
+  padding: 25px 0 34px;
 `;
 
 const ContentWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
   width: 90%;
   height: 100%;
 
   margin: 0 auto;
   padding-top: 10px;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 39px;
 `;
 
 const Title = styled.h2`
