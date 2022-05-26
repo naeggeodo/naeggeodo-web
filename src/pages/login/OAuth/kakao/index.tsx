0c6951bb3c95dac9e9f9c21624a0f25d95fb48cd
JYCHO <cjy0029@naver.com>
2022-05-26 16:58:56 +0900

    fix : 카카오 로그인 redirect url 수정

---
+++ src/pages/login/OAuth/kakao/index.tsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import Router, { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { ApiService } from '../../../../service/api';
+
+const index = () => {
+  const router = useRouter();
+  const code = router.asPath.split('=')[1];
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const res = await ApiService.postApi(
+  //       `/login/OAuth/kakao`,
+  //       JSON.stringify({ code }),
+  //     );
+  //     console.log(res);
+  //   })();
+  // }, []);
+
+  return (
+    <div
+      style={{
+        fontSize: '30px',
+        fontFamily: 'SpoqaBold',
+        textAlign: 'center',
+      }}>
+      카카오로그인
+    </div>
+  );
+};
+
+export default index;
