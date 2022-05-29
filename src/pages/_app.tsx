5e9d039905fe1112e430ae615c96b3c6f083b37c
JYCHO <cjy0029@naver.com>
2022-05-29 15:08:14 +0900

    feat : 헤드 태그 위치변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import GlobalStyle from '../styles/GlobalStyle';
 import { wrapper } from '../modules';
+import Head from 'next/head';
 
 const app = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <title>내꺼도 같이 시켜줘 🍔</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
