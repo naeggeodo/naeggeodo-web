76d9ee7ded8ea532d298fd6ba08832caa39df3cc
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-05-31 19:33:54 +0900

    fix : 스토리북 Provide로 감쌈

--- src/components/check-deposit/stories/ConvertToCompleteButton.stories.tsx
+++ src/components/check-deposit/stories/ConvertToCompleteButton.stories.tsx
@@ -1,5 +1,9 @@
 import { ComponentMeta } from '@storybook/react';
+import { Provider } from 'react-redux';
+import { configureStore } from '../../../modules';
 import ConvertToCompletedButton from '../ConvertToCompletedButton';
+
+const store = configureStore();
 
 export default {
   title: 'check-deposit/컴포넌트',
@@ -9,15 +13,17 @@ export default {
   },
   decorators: [
     (Story) => (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        }}>
-        <Story />
-      </div>
+      <Provider store={configureStore()}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}>
+          <Story />
+        </div>
+      </Provider>
     ),
   ],
 } as ComponentMeta<typeof ConvertToCompletedButton>;
