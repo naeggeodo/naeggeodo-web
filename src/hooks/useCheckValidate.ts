cd694aab02fa5727b367f3b885118d2450ba7163
JYCHO <cjy0029@naver.com>
2022-06-05 14:27:17 +0900

    refactor : 메인페이지 생성 버튼 클릭시 이동하게

--- src/hooks/useCheckValidate.ts
+++ src/hooks/useCheckValidate.ts
@@ -2,14 +2,16 @@ import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { TOKEN_NAME } from '../constant/Login';
 import { openLoginModal } from '../modules/modal/actions';
+import { useLoadLib } from './useLoadLib';
 
 export function useCheckValidate() {
-  const dispatch = useDispatch();
+  const { dispatch, router } = useLoadLib();
 
   const checkTokenAndRedirection = useCallback(() => {
     if (!localStorage.getItem(TOKEN_NAME.ACCESS_TOKEN)) {
       dispatch(openLoginModal());
     }
+    router.replace('/create');
   }, [dispatch]);
 
   return { checkTokenAndRedirection };
