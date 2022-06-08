7b637788bd52cf965d51bd53c2f066877a3279f4
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-06-08 21:11:54 +0900

    chore:건의하기 신고하기 팝업 스토리북 생성

---
+++ src/components/mypage/stories/FeedbakForm.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentMeta } from '@storybook/react';
+import { Provider } from 'react-redux';
+import { configureStore } from '../../../modules';
+import FeedbackForm from '../FeedbackForm';
+
+export default {
+  title: 'mypage/컴포넌트',
+  component: FeedbackForm,
+  decorators: [
+    (Story) => (
+      <Provider store={configureStore()}>
+        <div style={{ backgroundColor: '#fff', padding: '30px 0' }}>
+          <Story />
+        </div>
+      </Provider>
+    ),
+  ],
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof FeedbackForm>;
+
+const Template = (args) => <FeedbackForm {...args} />;
+
+export const FeedbackFormStory = Template.bind({});
+
+FeedbackFormStory.args = {};
+
+FeedbackFormStory.storyName = '건의하기 팝업';
