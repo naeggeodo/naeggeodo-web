00fc51bfd1e0754723b8427ad4fe9f1129ae9cd2
JYCHO <cjy0029@naver.com>
2022-06-09 13:03:48 +0900

    refactor : infinite scroll 훅으로 수정

--- src/components/main/MainTemplate.tsx
+++ src/components/main/MainTemplate.tsx
@@ -9,67 +9,38 @@ import PostCodeWebView from './PostCodeWebView';
 import LoginModal from '../login/LoginModalTemplate';
 
 import { CategoriesResponse } from '../../modules/main/types';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { RootState } from '../../modules';
 import { useCheckValidate } from '../../hooks/useCheckValidate';
 import { getBuildingCodeRequest } from '../../modules/search-post-code/actions';
-import { TOKEN_NAME } from '../../constant/Login';
 import NoItemText from './NoItemText';
 import { useLoadLoginStates } from '../../hooks/auth/useLoadLoginStates';
+import { useLoadLib } from '../../hooks/utils/useLoadLib';
+import { useInfiniteScroll } from '../../hooks/reder/useInfiniteScroll';
 
 const MainTemplate = ({
   foodCategories,
 }: {
   foodCategories: CategoriesResponse[];
 }) => {
-  const limit = 5;
-  const dispatch = useDispatch();
-  const { checkTokenAndRedirection, openWebView, closeWebView } =
-    useCheckValidate();
-  const [skip, setSkip] = useState(0);
-  const [dataList, setDataList] = useState([]);
-  const { user_id, accessToken } = useLoadLoginStates();
-
-  const target = useRef<HTMLDivElement>(null);
   const chatRooms = useSelector(
     (state: RootState) => state.mainPageState.chatRooms,
   );
+  const { dispatch } = useLoadLib();
+
+  const { checkTokenAndRedirection, openWebView, closeWebView } =
+    useCheckValidate();
+
+  const { target, dataList } = useInfiniteScroll(chatRooms);
+
+  const { user_id, accessToken } = useLoadLoginStates();
+
   const loginModalIsClicked = useSelector(
     (state: RootState) => state.modalStates.loginModalIsClicked,
   );
   const searchPostCodeIsOpen = useSelector(
     (state: RootState) => state.modalStates.searchPostCodeIsOpen,
   );
-
-  useEffect(() => {
-    if (chatRooms) {
-      const observer = new IntersectionObserver(callback, { threshold: 0.5 });
-      observer.observe(target.current);
-
-      return () => {
-        observer && observer.disconnect();
-      };
-    }
-  }, [chatRooms]);
-
-  useEffect(() => {
-    if (chatRooms && skip <= chatRooms.length) {
-      const arr = [];
-      for (let i = skip; i < chatRooms.length; i++) {
-        if (arr.length > limit) break;
-        arr.push(chatRooms[i]);
-      }
-      setDataList((prev) => [...prev, ...arr]);
-    }
-  }, [skip, chatRooms]);
-
-  const callback = async ([entry], observer: IntersectionObserver) => {
-    if (entry.isIntersecting) {
-      observer.unobserve(entry.target);
-      setSkip((prev) => prev + limit + 1);
-      observer.observe(entry.target);
-    }
-  };
 
   useEffect(() => {
     if (accessToken) {
