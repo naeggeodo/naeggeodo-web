47ec5f2aaeb31882e21c9f68eb6504f97f46bc21
Jayden <cjy0029@naver.com>
2022-06-21 15:37:49 +0900

    feat : modal 창 애니메이션 추가

--- src/components/login/LoginModalBox.tsx
+++ src/components/login/LoginModalBox.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { useLoadLib } from '../../hooks/utils/useLoadLib';
 import { closeLoginModal } from '../../modules/modal/actions';
@@ -6,6 +6,7 @@ import palette from '../../styles/palette';
 
 const LoginModalBox = () => {
   const { router, dispatch } = useLoadLib();
+  const modalRef = useRef(null);
 
   const moveLoginPage = useCallback(() => {
     router.replace('/login');
@@ -15,8 +16,12 @@ const LoginModalBox = () => {
     dispatch(closeLoginModal());
   }, [dispatch]);
 
+  useEffect(() => {
+    modalRef.current.style = 'top:50%; opacity:1';
+  }, []);
+
   return (
-    <ModalContainer>
+    <ModalContainer ref={modalRef}>
       <TitleWrapper>
         <Title>로그인이 필요합니다.</Title>
         <SubTitle>취소버튼을 누를시 서비스 이용이 어렵습니다.</SubTitle>
@@ -36,16 +41,18 @@ const ModalContainer = styled.div`
   justify-content: space-between;
 
   position: absolute;
-  top: 50%;
+  top: 30%;
   left: 50%;
 
   width: 88%;
   height: 244px;
+  opacity: 0;
   z-index: 2;
 
   padding: 30px;
   border-radius: 5px;
   transform: translate(-50%, -50%);
+  transition: 0.5s;
 
   box-shadow: 0 2px 7px rgba(0, 0, 0, 0.3);
   background-color: white;
