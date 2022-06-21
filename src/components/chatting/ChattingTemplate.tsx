e2fbc55d91bba854772074665d6a0c6732aa809a
Jayden <cjy0029@naver.com>
2022-06-21 21:34:40 +0900

    feat : 강퇴기능 완료

--- src/components/chatting/ChattingTemplate.tsx
+++ src/components/chatting/ChattingTemplate.tsx
@@ -1,27 +1,27 @@
-import React, { useState, useEffect, useRef, FormEvent } from "react";
-import styled from "styled-components";
-import { CompatClient, Stomp } from "@stomp/stompjs";
-import SockJS from "sockjs-client";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect, useRef, FormEvent } from 'react';
+import styled from 'styled-components';
+import { CompatClient, Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import { useSelector } from 'react-redux';
 
-import Header from "../chatting/Header";
-import GoInfoBtn from "../chatting/GoInfoBtn";
-import SubmitForm from "../chatting/SubmitForm";
-import ChatDrawer from "./ChatDrawer";
-import { RootState } from "../../modules";
-import ChattingList from "./ChattingList";
-import { useSelectLoginStates } from "../../hooks/select/useSelectLoginStates";
+import Header from '../chatting/Header';
+import GoInfoBtn from '../chatting/GoInfoBtn';
+import SubmitForm from '../chatting/SubmitForm';
+import ChatDrawer from './ChatDrawer';
+import { RootState } from '../../modules';
+import ChattingList from './ChattingList';
+import { useSelectLoginStates } from '../../hooks/select/useSelectLoginStates';
 import {
   changeCurrentCountInChatting,
   setCurrentChattingList,
   setImageListInChatting,
   setParticipatingUsers,
-} from "../../modules/chatting/actions";
-import DateFormatter from "../../utils/DateFormatter";
-import { useChat } from "../../hooks/useChat";
-import { useSelectChatRoomInfo } from "../../hooks/select/useSelectChatRoomInfo";
-import { useLoadLib } from "../../hooks/utils/useLoadLib";
-import ExitModalTemplate from "./ExitModalTemplate";
+} from '../../modules/chatting/actions';
+import DateFormatter from '../../utils/DateFormatter';
+import { useChat } from '../../hooks/useChat';
+import { useSelectChatRoomInfo } from '../../hooks/select/useSelectChatRoomInfo';
+import { useLoadLib } from '../../hooks/utils/useLoadLib';
+import ExitModalTemplate from './ExitModalTemplate';
 
 var stompClient: CompatClient;
 
@@ -35,13 +35,13 @@ const ChattingTemplate = () => {
   const { link, imgPath, currentCount, maxCount, title } =
     useSelectChatRoomInfo();
   const { chatRoomInfo, chattingList, nickname } = useSelector(
-    (state: RootState) => state.chattingRoomState
+    (state: RootState) => state.chattingRoomState,
   );
   const exitModalIsOpen = useSelector(
-    (state: RootState) => state.modalStates.exitModalIsOpen
+    (state: RootState) => state.modalStates.exitModalIsOpen,
   );
 
-  const [message, setMessage] = useState<string>("");
+  const [message, setMessage] = useState<string>('');
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
 
   const changeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -51,36 +51,36 @@ const ChattingTemplate = () => {
   useEffect(() => {
     chatListDivRef.current.scroll({
       top: scrollRef.current.offsetTop,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   }, [chattingList.messages]);
 
   const onError = (e) => {
     const errorMessage = e.headers.message;
     switch (errorMessage) {
-      case "SESSION_DUPLICATION":
-        alert("중복된 아이디로 접속하실 수 없습니다.");
-        location.replace("/chat-rooms");
+      case 'SESSION_DUPLICATION':
+        alert('중복된 아이디로 접속하실 수 없습니다.');
+        location.replace('/chat-rooms');
         return;
-      case "INVALID_STATE":
-        alert("입장할 수 없는 채팅방 입니다.");
-        location.replace("/chat-rooms");
+      case 'INVALID_STATE':
+        alert('입장할 수 없는 채팅방 입니다.');
+        location.replace('/chat-rooms');
         return;
-      case "BANNED_CHAT_USER":
-        alert("강제퇴장 조치로 인해 입장이 불가합니다.");
-        location.replace("/chat-rooms");
+      case 'BANNED_CHAT_USER':
+        alert('강제퇴장 조치로 인해 입장이 불가합니다.');
+        location.replace('/chat-rooms');
         return;
-      case "BAD_REQUEST":
-        alert("잘못된 요청입니다.");
-        location.replace("/");
+      case 'BAD_REQUEST':
+        alert('잘못된 요청입니다.');
+        location.replace('/');
         return;
-      case "UNAUTHORIZED":
-        alert("인증되지 않은 아이디입니다. 다시 로그인 해주세요.");
-        location.replace("/login");
+      case 'UNAUTHORIZED':
+        alert('인증되지 않은 아이디입니다. 다시 로그인 해주세요.');
+        location.replace('/login');
         return;
       default:
-        alert("잘못된 접근입니다.");
-        location.replace("/");
+        alert('잘못된 접근입니다.');
+        location.replace('/');
         return;
     }
   };
@@ -89,29 +89,30 @@ const ChattingTemplate = () => {
     const sendData = {
       chatMain_id: router.query.id,
       sender: user_id,
-      contents: "님이 입장하셨습니다.",
-      type: "WELCOME",
+      contents: '님이 입장하셨습니다.',
+      type: 'WELCOME',
       nickname,
     };
-    stompClient.send("/app/chat/enter", {}, JSON.stringify(sendData));
+    stompClient.send('/app/chat/enter', {}, JSON.stringify(sendData));
   }
 
   function exit() {
     const data = {
       chatMain_id: router.query.id,
       sender: user_id,
-      contents: "님이 퇴장하셨습니다.",
-      type: "EXIT",
+      contents: '님이 퇴장하셨습니다.',
+      type: 'EXIT',
       nickname,
     };
-    stompClient.send("/app/chat/exit", {}, JSON.stringify(data));
+    stompClient.send('/app/chat/exit', {}, JSON.stringify(data));
   }
 
   const connect = () => {
     // stompClient = Stomp.over(
     //   () => new SockJS(`${process.env.NEXT_PUBLIC_API_URL}/chat`)
     // );
-    const socket = new SockJS(`${process.env.NEXT_PUBLIC_API_URL}/chat`);
+
+    const socket: any = new SockJS(`${process.env.NEXT_PUBLIC_API_URL}/chat`);
     stompClient = Stomp.over(() => socket);
     stompClient.connect(
       {
@@ -120,11 +121,9 @@ const ChattingTemplate = () => {
         Authorization: `Bearer ${accessToken}`,
       },
       () => {
-        // TODO : 강퇴 기능
         const sessionId = /\/([^\\/]+)\/websocket/.exec(
-          socket._transport.url
+          socket._transport.url,
         )[1];
-        // console.log("sessionId", sessionId);
         stompClient.subscribe(
           `/topic/${router.query.id}`,
           (data) => {
@@ -140,29 +139,33 @@ const ChattingTemplate = () => {
             };
             dispatch(setCurrentChattingList(body));
 
-            if (newMessage.type === "CNT") {
+            if (newMessage.type === 'CNT') {
               dispatch(
                 changeCurrentCountInChatting(
-                  JSON.parse(newMessage.contents).currentCount
-                )
+                  JSON.parse(newMessage.contents).currentCount,
+                ),
               );
               dispatch(
-                setParticipatingUsers(JSON.parse(newMessage.contents).users)
+                setParticipatingUsers(JSON.parse(newMessage.contents).users),
               );
-            } else if (newMessage.type === "IMAGE") {
+            } else if (newMessage.type === 'IMAGE') {
               dispatch(setImageListInChatting(newMessage.contents));
             }
           },
-          { chatMain_id: router.query.id as string }
+          { chatMain_id: router.query.id as string },
         );
 
         stompClient.subscribe(`/user/queue/${sessionId}`, (e) => {
-          console.log(e.body);
+          const messageObj = JSON.parse(e.body);
+          if (messageObj.type === 'BAN') {
+            window.alert('강제퇴장 당하셨습니다.');
+            window.location.replace('/chat-rooms');
+          }
         });
 
         onEnter();
       },
-      onError
+      onError,
     );
   };
 
@@ -178,11 +181,11 @@ const ChattingTemplate = () => {
       chatMain_id: String(router.query.id),
       sender: user_id,
       contents: message,
-      type: "TEXT",
+      type: 'TEXT',
       nickname: nickname,
     };
     onSendMessage(stompClient, data);
-    setMessage("");
+    setMessage('');
   };
 
   const sendImage = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -196,12 +199,12 @@ const ChattingTemplate = () => {
         chatMain_id: String(router.query.id),
         sender: user_id,
         contents: result as string,
-        type: "IMAGE",
+        type: 'IMAGE',
         nickname: nickname,
       };
       onSendMessage(stompClient, data);
     };
-    e.target.value = "";
+    e.target.value = '';
   };
 
   useEffect(() => {
@@ -222,7 +225,7 @@ const ChattingTemplate = () => {
         setIsDrawerOpen={setIsDrawerOpen}
         title={title}
       />
-      {chatRoomInfo?.state !== "END" && chatRoomInfo?.user_id === user_id && (
+      {chatRoomInfo?.state !== 'END' && chatRoomInfo?.user_id === user_id && (
         <GoInfoBtn />
       )}
       <Content ref={chatListDivRef}>
