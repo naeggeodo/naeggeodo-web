ba0620de63462916c5fb6065c51300144b2ec364
Jayden <cjy0029@naver.com>
2022-06-24 14:12:48 +0900

    fix : 쿠키설정 수정

--- src/modules/login/sagas.ts
+++ src/modules/login/sagas.ts
@@ -67,7 +67,7 @@ function* naverLoginGenerator(action: getNaverTokenRequestAction) {
     cookies.set('address', response.data.address, {
       path: '/',
     });
-    // window.location.href = "/";
+    window.location.href = '/';
   } catch (error) {
     console.log(error);
   }
